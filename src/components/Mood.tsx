9f1680f7255ed4392f502fec5eb79b746fa0565b
Mr-Quin <hr82003@gmail.com>
2020-09-05 23:22:15 -0700

    add some chord progression for testing

--- src/components/Mood.tsx
+++ src/components/Mood.tsx
@@ -1,51 +1,78 @@
 import React, { createContext, useContext, useEffect, useState } from 'react'
-import { Progression, Chord } from "@tonaljs/tonal";
-import { Transport } from "tone";
+import { Progression, Chord, Note } from '@tonaljs/tonal'
+import { Transport } from 'tone'
 
 interface Mood {
-    bpm: number;
-    keys: Array<string>;
-    chordName: string;
+    bpm: number
+    keys: Array<string>
+    chordName: string
 }
 
 const MoodContext = createContext<Partial<Mood>>({})
 
-const MoodProvider: React.FunctionComponent<any> = ({...props}) => {
-    const [chord, setChord] = useState<any>({});
-    const [bpm, setBpm] = useState<number>(80);
+const MoodProvider: React.FunctionComponent<any> = ({ ...props }) => {
+    const [chord, setChord] = useState<any>({})
+    const [bpm, setBpm] = useState<number>(120)
 
     // const chords = Progression.fromRomanNumerals("C", ['Im', 'Vm', 'VIb', 'IIIb', 'IVm', 'Im', 'II', 'V']);
-    const chords = Progression.fromRomanNumerals("C", ['I', 'V', 'VIm', 'IIIm', 'IV', 'I', 'IV', 'V']);
+    // const chords = Progression.fromRomanNumerals('C', [  // Canon
+    //     'I',
+    //     'V',
+    //     'VIm',
+    //     'IIIm',
+    //     'IV',
+    //     'I',
+    //     'IV',
+    //     'V',
+    // ])
+    // const chords = Progression.fromRomanNumerals('C', [
+    //     // Nier
+    //     'IM7',
+    //     'VIIm7',
+    //     'VIm7',
+    //     'VM7',
+    //     'IVM7',
+    //     'IIIm7',
+    //     'IIIbM7',
+    //     'II7',
+    // ])
+    const chords = Progression.fromRomanNumerals('F', [
+        // FF
+        'I',
+        'V',
+        'IV',
+        'IIIm',
+        'IV',
+        'II',
+        'Vsus4',
+        'V',
+    ])
 
     useEffect(() => {
-        let i = 0;
-        Transport.scheduleRepeat(time => {
+        let i = 0
+        Transport.scheduleRepeat((time) => {
             setChord(Chord.get(chords[i]))
             i += 1
-            if (i >= chords.length) i = 0;
-        }, "1n");
-        Transport.start();
-        return (() => {
+            if (i >= chords.length) i = 0
+        }, '1n')
+        Transport.start()
+        return () => {
             Transport.stop()
-        })
+        }
     }, [])
 
     useEffect(() => {
-        Transport.bpm.value = bpm;
+        Transport.bpm.value = bpm
     }, [bpm])
 
     const value = {
         bpm: bpm,
         keys: chord.notes,
         chordName: chord.name,
-        setBpm: setBpm
+        setBpm: setBpm,
     }
 
-    return (
-        <MoodContext.Provider value={value}>
-            {props.children}
-        </MoodContext.Provider>
-    )
+    return <MoodContext.Provider value={value}>{props.children}</MoodContext.Provider>
 }
 
 const useMood = () => {
@@ -54,4 +81,4 @@ const useMood = () => {
     return context
 }
 
-export { MoodContext, useMood, MoodProvider };
+export { MoodContext, useMood, MoodProvider }
