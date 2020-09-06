c6211a6f9fd864c4aa57120ea8c27f6b8cb2fcf9
Mr-Quin <hr82003@gmail.com>
2020-09-05 23:22:02 -0700

    slight changes to util functions

--- src/utils/utils.tsx
+++ src/utils/utils.tsx
@@ -1,26 +1,25 @@
-import { Point } from "../Types";
+import { Point } from '../Types'
 
 export const distPoint = (pt1: Point, pt2: Point) => {
-    const a = Math.pow(pt1.x - pt2.x, 2);
-    const b = Math.pow(pt1.y - pt2.y, 2);
-    return Math.sqrt(a + b);
+    const a = Math.pow(pt1.x - pt2.x, 2)
+    const b = Math.pow(pt1.y - pt2.y, 2)
+    return Math.sqrt(a + b)
 }
 
 export const midPoint = (pt1: Point, pt2: Point): Point => {
     return {
         x: (pt1.x + pt2.x) / 2,
-        y: (pt1.y + pt2.y) / 2
+        y: (pt1.y + pt2.y) / 2,
     }
 }
 
 export const randomFromArray = (arr: Array<any>): any => {
-    const randomIndex = Math.random() * arr.length << 0;
-    return arr[randomIndex];
+    return arr[(Math.random() * arr.length) << 0]
 }
 
-export const mapValue: (...args: Array<number>) => number = (value, low1, high1, low2, high2, ceil) => {
-    if (ceil && value > high1) {
+export const mapValue: (...args: Array<number>) => number = (value, low1, high1, low2, high2) => {
+    if (value > high1) {
         return high2
     }
-    return low2 + (high2 - low2) * (value - low1) / (high1 - low1);
-};
+    return low2 + ((high2 - low2) * (value - low1)) / (high1 - low1)
+}
