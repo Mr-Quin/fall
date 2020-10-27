91ca10f30aca7ce15a0921ef94ab67ceab3c04db
Mr-Quin <hr82003@gmail.com>
2020-10-26 20:37:41 -0700

    Update config-overrides.js

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,4 @@
 module.exports = function override(config, env) {
-    //do stuff with the webpack config...
     config.module.rules[2].oneOf.splice(1, 0, {
         test: /\.(gltf|glb)$/i,
         loader: require.resolve('file-loader'),
@@ -7,6 +6,5 @@ module.exports = function override(config, env) {
             name: '[hash].[ext]',
         },
     })
-    console.log(config.module.rules[2].oneOf)
     return config
 }
