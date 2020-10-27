55a0b818cce6c0073ebdea22639840f292caa727
Mr-Quin <hr82003@gmail.com>
2020-10-26 20:38:00 -0700

    add assets

---
+++ src/assets/index.ts
@@ -0,0 +1,7 @@
+import star from './star.png'
+import starRound from './star_round.jpg'
+import flare from './flare.jpg'
+import starGlb from './star.glb'
+import ding from './ding.wav'
+
+export { starGlb, star, starRound, flare, ding }
