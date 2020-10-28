7a0a83222b24bfb843d87b0409ac91cf02205237
Mr-Quin <hr82003@gmail.com>
2020-10-28 01:18:52 -0700

    splitted into more stores

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -3,40 +3,40 @@ import * as BABYLON from '@babylonjs/core'
 import { Nullable } from '@babylonjs/core'
 
 type StoreState = {
+    sceneReady: boolean
+    animationFinished: boolean
+    fallen: boolean
     statics: {
         scene: Nullable<BABYLON.Scene>
         canvas: Nullable<HTMLCanvasElement>
         camera: Nullable<BABYLON.Camera>
     }
-    mutations: {
-        ready: boolean
-    }
+    mutations: {}
     actions: {
         init: (
             scene: Nullable<BABYLON.Scene>,
             canvas: Nullable<HTMLCanvasElement>,
             camera: Nullable<BABYLON.Camera>
         ) => void
-        setReady: () => void
         fall: () => void
     }
 }
 
 const useStore = create<StoreState>((set, get) => ({
+    sceneReady: false,
+    animationFinished: false,
+    fallen: false,
     statics: {
         scene: null,
         canvas: null,
         camera: null,
     },
-    mutations: {
-        ready: false,
-    },
+    mutations: {},
     actions: {
         init: (scene, canvas, camera) => {
             set((state) => ({ statics: { scene: scene, canvas: canvas, camera: camera } }))
         },
-        setReady: () => void (get().mutations.ready = true),
-        fall: () => {},
+        fall: () => void 0,
     },
 }))
 
