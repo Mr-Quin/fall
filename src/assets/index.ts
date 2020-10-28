4de20c3661ae4a21d377b18f2db708563ae79a52
Mr-Quin <hr82003@gmail.com>
2020-10-28 01:20:24 -0700

    Update index.ts

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -2,6 +2,5 @@ import star from './star.png'
 import starRound from './star_round.jpg'
 import flare from './flare.jpg'
 import starGlb from './star.glb'
-import ding from './ding.wav'
 
-export { starGlb, star, starRound, flare, ding }
+export { starGlb, star, starRound, flare }
