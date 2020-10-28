91ddc7ead42416da6c7c0c546c588065078b1d82
Mr-Quin <hr82003@gmail.com>
2020-10-28 15:59:40 -0700

    reduce terxture size

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,6 +1,6 @@
 import star from './star.png'
-import starRound from './star_round.jpg'
-import flare from './flare.jpg'
+import starRound from './star_round.png'
+import flare from './flare.png'
 import starGlb from './star.glb'
 
 export { starGlb, star, starRound, flare }
