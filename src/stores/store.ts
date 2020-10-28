dc764a4d3a20e801fe729d3c59585f3b01d7c898
Mr-Quin <hr82003@gmail.com>
2020-10-28 16:00:42 -0700

    Update store.ts

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -1,24 +1,31 @@
 import create from 'zustand'
-import * as BABYLON from '@babylonjs/core'
-import { Nullable } from '@babylonjs/core'
+import { AbstractMesh, Camera, Nullable, Scene } from '@babylonjs/core'
+import { randomFromArray, randomRange } from '../utils/utils'
+import { Chord } from '@tonaljs/chord'
+import { Player } from 'soundfont-player'
 
 type StoreState = {
     sceneReady: boolean
     animationFinished: boolean
     fallen: boolean
     statics: {
-        scene: Nullable<BABYLON.Scene>
+        scene: Nullable<Scene>
         canvas: Nullable<HTMLCanvasElement>
-        camera: Nullable<BABYLON.Camera>
+        camera: Nullable<Camera>
     }
-    mutations: {}
+    mutations: {
+        steps: AbstractMesh[]
+        chord: Nullable<Chord>
+        player: Nullable<Player>
+    }
     actions: {
         init: (
-            scene: Nullable<BABYLON.Scene>,
+            scene: Nullable<Scene>,
             canvas: Nullable<HTMLCanvasElement>,
-            camera: Nullable<BABYLON.Camera>
+            camera: Nullable<Camera>
         ) => void
         fall: () => void
+        playTone: () => void
     }
 }
 
@@ -31,12 +38,23 @@ const useStore = create<StoreState>((set, get) => ({
         canvas: null,
         camera: null,
     },
-    mutations: {},
+    mutations: {
+        steps: [],
+        chord: null,
+        player: null,
+    },
     actions: {
         init: (scene, canvas, camera) => {
             set((state) => ({ statics: { scene: scene, canvas: canvas, camera: camera } }))
         },
         fall: () => void 0,
+        playTone: () => {
+            const notes = get().mutations.chord!.notes
+            const note = randomFromArray(notes)
+            const octave = randomRange(6, 8, true)
+            const player = get().mutations.player
+            player && player.play(`${note}${octave}`)
+        },
     },
 }))
 
