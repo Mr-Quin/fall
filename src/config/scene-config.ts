0f38d7c0623f53dcc52f4fd49c243c0edd71239f
Mr-Quin <hr82003@gmail.com>
2020-10-29 00:37:43 -0700

    added function to prnt art to console

--- src/config/scene-config.ts
+++ src/config/scene-config.ts
@@ -1,1 +1,21 @@
+export const logArt = () => {
+    console.log(
+        `%c
+                    *****/     .-._.---'  /\\         .-.        .-. 
+         ,       *******//     (_) /   _  / |        / (_)      / (_)
+        ,,,,,/*********//         /--.(  /  |  .    /          /       
+        ,,,,,,,,,,,,,*//         /     \`/.__|_.'   /          /        
+      ,,,,,,,,,,,,,,*///      .-/  .:' /    |   .-/.    .-..-/.    .-.
+  ,,,,,,,,,,,,,,,,,*///      (_/  (__.'     \`-'(_/ \`-._.  (_/ \`-._.
+      ,,,,,,,,,,,,,,//        
+        ,,,,,,,,,,,,,,        
+         ,,,,                
+          ,,                
+               %cPowered by React.js and Babylon.js`,
+        `color: ${defaults.starColor};`,
+        "background-color:black; padding:5px; font-family: 'Poiret One', cursive; font-size: 1.5em"
+    )
+    ;(window as any).logArt = logArt || {}
+}
+
 export const sceneConfig = {}
