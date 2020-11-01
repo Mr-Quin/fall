c01c109fe1137c81bb1b60895a1abc5e513b66f1
Mr-Quin <hr82003@gmail.com>
2020-11-01 15:38:54 -0800

    no longer stores the firebase app in state

--- src/hooks/useFirebase.ts
+++ src/hooks/useFirebase.ts
@@ -1,18 +1,13 @@
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import firebase from 'firebase/app'
 import 'firebase/analytics'
 import { firebaseConfig } from '../config/firebase-config'
 
 const useFirebase = () => {
-    const [firebaseApp, setFirebaseApp] = useState<firebase.app.App>()
-
     useEffect(() => {
-        const app = firebase.initializeApp(firebaseConfig)
+        firebase.initializeApp(firebaseConfig)
         firebase.analytics()
-        setFirebaseApp(app)
     }, [])
-
-    return firebaseApp
 }
 
 export default useFirebase
