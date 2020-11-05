7f1700a25efb21eaf0726b05949c83c2f8b5f85f
Mr-Quin <hr82003@gmail.com>
2020-11-04 19:49:00 -0800

    created new helper files to clean up code

---
+++ src/helpers/particleHelper.ts
@@ -0,0 +1,187 @@
+import {
+    Color4,
+    GPUParticleSystem,
+    NoiseProceduralTexture,
+    ParticleSystem,
+    ProceduralTexture,
+    Texture,
+    Vector3,
+    Scene,
+} from '@babylonjs/core'
+import { colors } from '../config/scene-config'
+
+type StarFieldParticleSystemOptions = {
+    capacity: number
+    texture: Texture
+    noiseTexture: ProceduralTexture
+    noiseStrength?: Vector3
+    color1: Color4
+    color2: Color4
+    maxSize?: number
+    minSize?: number
+    emitterSize?: number
+}
+
+type AmbientParticleSystemOptions = Omit<StarFieldParticleSystemOptions, 'emitterSize'>
+
+const createCollisionParticleSystem = (texture: Texture, capacity: number, scene: Scene) => {
+    const ps = new ParticleSystem('collision-ps', capacity, scene)
+    ps.createPointEmitter(Vector3.Zero(), Vector3.Zero())
+    ps.particleTexture = texture
+    ps.color1 = Color4.FromHexString(colors.starColorPrimary)
+    ps.color2 = Color4.FromHexString(colors.starColorSecondary)
+    ps.colorDead = Color4.FromHexString(colors.transparent)
+    ps.minSize = 0.2
+    ps.maxSize = 0.4
+    ps.minLifeTime = 0.7
+    ps.maxLifeTime = 1
+    ps.minEmitPower = 1
+    ps.maxEmitPower = 1
+    ps.addVelocityGradient(0, 3, 6)
+    ps.addVelocityGradient(1.0, -0.5, -1)
+    ps.addDragGradient(0, 0.3)
+    ps.addDragGradient(1, 0.7)
+    ps.minAngularSpeed = -Math.PI * 2
+    ps.maxAngularSpeed = Math.PI * 2
+    ps.direction1 = new Vector3(-1, -1, -1)
+    ps.direction2 = new Vector3(1, 1, 1)
+    ps.gravity = new Vector3(0, 0, 0)
+    ps.manualEmitCount = 0
+    ps.emitRate = 0
+    return ps
+}
+
+const createStarFieldParticleSystem = (options: StarFieldParticleSystemOptions, scene: Scene) => {
+    const {
+        capacity,
+        texture,
+        noiseTexture,
+        noiseStrength = new Vector3(0.5, 0.5, 0.5),
+        color1,
+        color2,
+        maxSize = 0.5,
+        minSize = 0.05,
+        emitterSize = 40,
+    } = options
+    const ps = new GPUParticleSystem(
+        'star-field',
+        { capacity: capacity, randomTextureSize: 1024 },
+        scene
+    )
+    ps.noiseTexture = noiseTexture
+    ps.noiseStrength = noiseStrength
+    ps.createBoxEmitter(
+        Vector3.Zero(),
+        Vector3.Zero(),
+        new Vector3(-emitterSize / 2, -emitterSize / 2, -emitterSize / 2),
+        new Vector3(emitterSize / 2, emitterSize / 2, emitterSize / 2)
+    )
+    ps.particleTexture = texture
+    ps.addColorGradient(0, Color4.FromHexString(colors.transparent))
+    ps.addColorGradient(0.1, color1)
+    ps.addColorGradient(0.5, color2)
+    ps.addColorGradient(0.9, color2)
+    ps.addColorGradient(1, Color4.FromHexString(colors.transparent))
+    ps.minSize = minSize
+    ps.maxSize = maxSize
+    ps.minLifeTime = 3
+    ps.maxLifeTime = 6
+    ps.preWarmCycles = 600
+    ps.minEmitPower = -1
+    ps.maxEmitPower = 1
+    ps.minAngularSpeed = -Math.PI
+    ps.maxAngularSpeed = Math.PI
+    ps.gravity = new Vector3(0, 0, 0)
+    ps.emitRate = capacity / 3
+    return ps
+}
+
+const createAmbientParticleSystem = (options: AmbientParticleSystemOptions, scene: Scene) => {
+    const {
+        capacity,
+        texture,
+        noiseTexture,
+        noiseStrength = new Vector3(1, 1, 1),
+        color1,
+        color2,
+        maxSize = 0.05,
+        minSize = 0.05,
+    } = options
+    const ps = new GPUParticleSystem(
+        'ambient-ps',
+        { capacity: capacity, randomTextureSize: 1024 },
+        scene
+    )
+    ps.noiseTexture = noiseTexture
+    ps.noiseStrength = noiseStrength
+    ps.createBoxEmitter(
+        new Vector3(0, 1, 0),
+        Vector3.Zero(),
+        new Vector3(-10, 2, -10),
+        new Vector3(10, -2, 10)
+    )
+    ps.particleTexture = texture
+    ps.minSize = minSize
+    ps.maxSize = maxSize
+    ps.addColorGradient(0, Color4.FromHexString(colors.transparent))
+    ps.addColorGradient(0.2, color1)
+    ps.addColorGradient(0.8, color2)
+    ps.addColorGradient(1, Color4.FromHexString(colors.transparent))
+    ps.addSizeGradient(0, 0.2)
+    ps.addSizeGradient(0.3, 0.02)
+    ps.addSizeGradient(0.7, 0.15)
+    ps.addSizeGradient(1, 0.02)
+    ps.maxEmitPower = 0
+    ps.minEmitPower = 0
+    ps.minLifeTime = 3
+    ps.maxLifeTime = 7
+    ps.emitRate = capacity / 5
+    return ps
+}
+
+const createTrailParticleSystem = (capacity, texture, scene) => {
+    const gpuParticleSystem = new GPUParticleSystem(
+        'trail-ps',
+        { capacity: capacity, randomTextureSize: 1024 },
+        scene
+    )
+    const noiseTexture = new NoiseProceduralTexture('perlin', 256, scene)
+    noiseTexture.animationSpeedFactor = 2
+    noiseTexture.brightness = 0.5
+    noiseTexture.octaves = 5
+    gpuParticleSystem.noiseTexture = noiseTexture
+
+    gpuParticleSystem.noiseStrength = new Vector3(1, 1, 1)
+    gpuParticleSystem.emitRate = 5
+    gpuParticleSystem.minLifeTime = 0.2
+    gpuParticleSystem.maxLifeTime = 0.4
+    gpuParticleSystem.createPointEmitter(Vector3.Zero(), Vector3.Zero())
+    gpuParticleSystem.particleTexture = texture
+    gpuParticleSystem.updateSpeed = 1 / 100
+    gpuParticleSystem.maxInitialRotation = Math.PI
+    gpuParticleSystem.maxInitialRotation = -Math.PI
+
+    gpuParticleSystem.addColorGradient(0, new Color4(0, 0, 0, 0))
+    gpuParticleSystem.addColorGradient(
+        0.05,
+        new Color4(0.4, 0.3, 0.8, 1),
+        new Color4(0.2, 0.9, 0.8, 1)
+    )
+    gpuParticleSystem.addColorGradient(0.8, new Color4(0.8, 0.3, 0.2, 1))
+    gpuParticleSystem.addColorGradient(1, new Color4(0, 0, 0, 0))
+
+    gpuParticleSystem.addSizeGradient(0, 0.8)
+    gpuParticleSystem.addSizeGradient(1, 0)
+
+    gpuParticleSystem.maxEmitPower = 0
+    gpuParticleSystem.minEmitPower = 0
+
+    return gpuParticleSystem
+}
+
+export {
+    createAmbientParticleSystem,
+    createCollisionParticleSystem,
+    createTrailParticleSystem,
+    createStarFieldParticleSystem,
+}
---
+++ src/helpers/sceneHelpers.ts
@@ -0,0 +1,184 @@
+import {
+    AbstractMesh,
+    Animation,
+    Color3,
+    CubicEase,
+    EasingFunction,
+    EngineInstrumentation,
+    HemisphericLight,
+    Mesh,
+    PhysicsImpostor,
+    PhysicsJoint,
+    SceneInstrumentation,
+    Vector3,
+} from '@babylonjs/core'
+
+const toggleOverlay = async (scene) => {
+    await import('@babylonjs/inspector')
+    await import('@babylonjs/core/Debug/debugLayer')
+    const showing = scene.debugLayer.isVisible()
+    await scene.debugLayer.show({ overlay: !showing })
+}
+
+const enableDebugMetrics = async (scene) => {
+    const BABYLONGUI = await import('@babylonjs/gui')
+    const advancedTexture = BABYLONGUI.AdvancedDynamicTexture.CreateFullscreenUI('UI')
+    const stackPanel = new BABYLONGUI.StackPanel()
+    stackPanel.verticalAlignment = BABYLONGUI.Control.VERTICAL_ALIGNMENT_TOP
+    stackPanel.topInPixels = 30
+    stackPanel.isVertical = true
+    advancedTexture.addControl(stackPanel)
+    const frameTime = new BABYLONGUI.TextBlock()
+    const averageFrameTime = new BABYLONGUI.TextBlock()
+    const shaderTime = new BABYLONGUI.TextBlock()
+    const shaderCount = new BABYLONGUI.TextBlock()
+    const physicsTime = new BABYLONGUI.TextBlock()
+    const fps = new BABYLONGUI.TextBlock()
+
+    const applyTextStyles = (textBlocks) =>
+        void textBlocks.forEach((textBlock) => {
+            textBlock.text = ''
+            textBlock.color = 'white'
+            textBlock.fontSize = 16
+            textBlock.height = '30px'
+            stackPanel.addControl(textBlock)
+        })
+
+    applyTextStyles([frameTime, averageFrameTime, shaderTime, shaderCount, physicsTime, fps])
+
+    const engine = scene!.getEngine()
+    const engineInstrumentation = new EngineInstrumentation(engine)
+    const sceneInstrumentation = new SceneInstrumentation(scene)
+
+    engineInstrumentation.captureGPUFrameTime = true
+    engineInstrumentation.captureShaderCompilationTime = true
+    sceneInstrumentation.capturePhysicsTime = true
+
+    scene.registerBeforeRender(() => {
+        frameTime.text = `Current frame time (GPU): ${(
+            engineInstrumentation.gpuFrameTimeCounter.current * 0.000001
+        ).toFixed(2)} ms`
+        averageFrameTime.text = `Average frame time (GPU): ${(
+            engineInstrumentation.gpuFrameTimeCounter.average * 0.000001
+        ).toFixed(2)} ms`
+        shaderTime.text = `Total shader compilation time: ${engineInstrumentation.shaderCompilationTimeCounter.total.toFixed(
+            2
+        )} ms`
+        shaderCount.text = `Compiler shaders count: ${engineInstrumentation.shaderCompilationTimeCounter.count}`
+        physicsTime.text = `Current physics time: ${sceneInstrumentation.physicsTimeCounter.current.toFixed(
+            2
+        )} ms`
+        fps.text = `FPS: ${engine.getFps().toFixed()}`
+    })
+}
+
+type ChainOptions = {
+    count: number
+    distance: number
+    mass: number
+    hideChains: boolean
+}
+
+const createChain = (startMesh, endMesh, scene, options) => {
+    const defaultOptions: ChainOptions = {
+        count: 3,
+        distance: 0.5,
+        mass: 1,
+        hideChains: false,
+    }
+
+    if (options) {
+        for (const key in options) {
+            defaultOptions[key] = options[key]
+        }
+    }
+
+    const { count, distance, mass, hideChains } = defaultOptions
+
+    const links: AbstractMesh[] = []
+    const jointData = {
+        mainPivot: new Vector3(0, -distance, 0),
+        connectedPivot: new Vector3(0, distance, 0),
+    }
+    for (let i = 0; i < count; i++) {
+        const link = Mesh.CreateBox(`joint-box-${i}`, 0.3, scene!).convertToUnIndexedMesh()
+        link.isVisible = !hideChains
+
+        link.position = new Vector3(
+            startMesh.position.x,
+            startMesh.position.y - (i + 1) * distance,
+            startMesh.position.z
+        )
+
+        const physicsImposter = setPhysicsImposter(link, PhysicsImpostor.BoxImpostor, scene, {
+            mass: mass,
+        })
+
+        links.push(link)
+
+        if (i > 0) {
+            links[i - 1].physicsImpostor!.createJoint(
+                physicsImposter,
+                PhysicsJoint.BallAndSocketJoint,
+                jointData
+            )
+        }
+    }
+
+    startMesh.physicsImpostor!.createJoint(
+        links[0].physicsImpostor!,
+        PhysicsJoint.BallAndSocketJoint,
+        jointData
+    )
+
+    endMesh.position.copyFrom(links[links.length - 1].position)
+
+    links[links.length - 1].physicsImpostor!.createJoint(
+        endMesh.physicsImpostor!,
+        PhysicsJoint.BallAndSocketJoint,
+        jointData
+    )
+
+    return links
+}
+
+const createTransition = (object, prop, to, speed) => {
+    const ease = new CubicEase()
+    ease.setEasingMode(EasingFunction.EASINGMODE_EASEINOUT)
+
+    return new Promise((res, rej) => {
+        Animation.CreateAndStartAnimation(
+            'transition-animation',
+            object,
+            prop,
+            speed,
+            120,
+            object[prop],
+            to,
+            0,
+            ease,
+            res
+        )
+    })
+}
+
+const createLight = (scene) => {
+    const light = new HemisphericLight('light', new Vector3(0, 1, 0), scene)
+    light.intensity = 0.5
+    light.range = 1
+    light.diffuse = new Color3(0.05, 0, 0.2)
+    return light
+}
+
+const setPhysicsImposter = (object, type, scene, options) => {
+    return (object.physicsImpostor = new PhysicsImpostor(object, type, options, scene))
+}
+
+export {
+    toggleOverlay,
+    enableDebugMetrics,
+    createChain,
+    createLight,
+    createTransition,
+    setPhysicsImposter,
+}
