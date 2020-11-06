f89ff708daaaf45da29ffeac7924223005c04c5c
Mr-Quin <hr82003@gmail.com>
2020-11-06 10:41:48 -0800

    added connection to indexeddb using Dexie

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -1,15 +1,17 @@
 import create from 'zustand'
-import { AbstractMesh, Camera, Color4, Nullable, Scene } from '@babylonjs/core'
+import { AbstractMesh, Camera, Color4, Nullable, Scene, Vector3 } from '@babylonjs/core'
 import { getValidNote, randomRange } from '../utils/utils'
 import { fromMidi } from '@tonaljs/note'
 import { Player } from 'soundfont-player'
 import { constants, colors } from '../config/scene-config'
 import { PianoGenie } from '../helpers/PianoGenie'
+import StepDatabase, { Step } from '../helpers/StepDatabase'
 
 type StoreState = {
     sceneReady: boolean
     animationFinished: boolean
     fallen: boolean
+    db: StepDatabase
     statics: {
         scene: Nullable<Scene>
         canvas: Nullable<HTMLCanvasElement>
@@ -28,18 +30,21 @@ type StoreState = {
             canvas: Nullable<HTMLCanvasElement>,
             camera: Nullable<Camera>
         ) => void
+        appendDb: (data: Step) => void
         fall: () => void
-        playTone: () => void
+        playTone: (position: Vector3) => void
         randomizeColor: () => void
     }
 }
 
+const date = new Date()
 const { LOWEST_INSTRUMENT_MIDI_NUMBER, LOWEST_PIANO_MIDI_NUMBER, GENIE_TEMPERATURE } = constants
 
 const useStore = create<StoreState>((set, get) => ({
     sceneReady: false,
     animationFinished: false,
     fallen: false,
+    db: new StepDatabase(),
     statics: {
         scene: null,
         canvas: null,
@@ -57,7 +62,13 @@ const useStore = create<StoreState>((set, get) => ({
             set((state) => ({ statics: { scene: scene, canvas: canvas, camera: camera } }))
         },
         fall: () => void 0,
-        playTone: () => {
+        appendDb: (data) => {
+            const db = get().db
+            db.transaction('rw', db.steps, async () => {
+                await db.steps.add(data)
+            })
+        },
+        playTone: (position) => {
             const genie = get().mutations.genie
             const player = get().mutations.player
             if (genie === null || player === null) return
@@ -68,6 +79,7 @@ const useStore = create<StoreState>((set, get) => ({
 
             player.play(fromMidi(validNote))
 
+            get().actions.appendDb({ position: position, note: validNote, time: date.getTime() })
             get().actions.randomizeColor()
         },
         randomizeColor: () => {
@@ -75,7 +87,7 @@ const useStore = create<StoreState>((set, get) => ({
                 ({ mutations }) =>
                     void (mutations.colorTarget = new Color4(
                         randomRange(0, 0.07),
-                        randomRange(0, 0.07),
+                        randomRange(0, 0.03),
                         randomRange(0, 0.07),
                         1
                     )) as any
