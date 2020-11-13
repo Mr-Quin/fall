ded38f6e76682419c11c22bca48767d89a1958a1
Mr-Quin <hr82003@gmail.com>
2020-11-12 18:08:00 -0800

    added some comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect, Suspense } from 'react'
 import useStore from './stores/store'
+import shallow from 'zustand/shallow'
 
 import TitleScreen from './components/TitleScreen'
-import Genie from './components/Genie'
 import Footer from './components/Footer'
 import LoadingScreen from './components/LoadingScreen'
+import Ui from './components/Ui'
 import { WebGL2Error } from './components/ErrorPage'
 
 import withFade from './styles/withFade'
@@ -21,16 +22,20 @@ const TitleWrapper = withFade(FullScreen)
 
 const LazyBabylonScene = React.lazy(() => import('./components/3d/SceneViewer'))
 
-const selector = (state) => [state.sceneReady, state.animationFinished, state.fallen]
-const { backgroundColor } = colors
+const selector = (state) => [state.sceneReady, state.titleAnimationFinished, state.fallen]
+const { BACKGROUND_COLOR } = colors
 
 const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
     navigator.userAgent
 )
 const supportWebGL2 = !!document.createElement('canvas').getContext('webgl2')
 
+// if (process.env.NODE_ENV !== 'development') {
+//     console.debug = () => {}
+// }
+
 const App = () => {
-    const [sceneReady, animationFinished, fallen] = useStore(selector)
+    const [sceneReady, titleAnimationFinished, fallen] = useStore(selector, shallow)
     const [renderOverlayScreen, toggleRenderOverlayScreen] = useToggle(true)
 
     useFirebase()
@@ -38,32 +43,35 @@ const App = () => {
 
     useEffect(() => {
         if (!fallen) return
+        // unmount the entire overlay section when the star has fallen
         const timeout = setTimeout(toggleRenderOverlayScreen, 2000)
         return () => clearTimeout(timeout)
     }, [fallen])
 
     return (
-        <FullScreen background={backgroundColor}>
+        <FullScreen background={BACKGROUND_COLOR}>
             {supportWebGL2 ? (
                 <>
                     {renderOverlayScreen && (
-                        <TitleWrapper show={!fallen} transition passPointer>
+                        <TitleWrapper show={!fallen} passPointer>
                             <LoadingBg
-                                show={!animationFinished}
-                                background={backgroundColor}
-                                transition
+                                // fadeout the opaque background after title animation finishes
+                                show={!titleAnimationFinished}
+                                background={BACKGROUND_COLOR}
                                 duration={'3s'}
                                 passPointer
                             />
                             <LoadingScreen show={!sceneReady} />
-                            {sceneReady && <TitleScreen show />}
+                            {sceneReady && <TitleScreen />}
                         </TitleWrapper>
                     )}
+
+                    <Ui />
+
                     <Suspense fallback={null}>
-                        <LazyBabylonScene>
-                            <Genie />
-                        </LazyBabylonScene>
+                        <LazyBabylonScene />
                     </Suspense>
+
                     {process.env.NODE_ENV === 'development' ? (
                         <Footer>Development Build</Footer>
                     ) : null}
